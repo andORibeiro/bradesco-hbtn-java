bcaa07149fd813af841cff984c9e36d540642ac2
Anderson Ribeiro <anderson.corporative@outlook.com>
2025-07-31 00:09:15 -0300

    movendo FormaGeometrica

---
+++ Heranca/quadrado/FormaGeometrica.java
@@ -0,0 +1,11 @@
+public class FormaGeometrica {
+    
+    public double area() throws Exception {
+        throw new java.lang.UnsupportedOperationException("Funcao area nao implementada");
+    }
+
+    @Override
+    public String toString() {
+        return "FormaGeometrica";
+    }
+}
